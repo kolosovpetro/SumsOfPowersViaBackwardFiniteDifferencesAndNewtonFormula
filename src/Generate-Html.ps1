daa07573807c31f1c1088cfcc431ce0b21e9e07f
kolosovpetro <kolosovp94@gmail.com>
2026-01-24 12:55:45 +0100

    update pandoc script

--- src/Generate-Html.ps1
+++ src/Generate-Html.ps1
@@ -9,12 +9,6 @@ pandoc "$LatexFileName.tex" --mathjax --standalone --citeproc `
     --bibliography="$LatexFileName.bib" `
     --csl=chicago-author-date.csl -o index.html
 
-$EncodingScriptPath = "$PSScriptRoot/../scripts/Test-Encoding.ps1"
-
-& $EncodingScriptPath -Autofix
-
-Write-Host "Fix encoding is complete." -ForegroundColor Green
-
 Write-Host "Updating metadata ..."
 
 $MetadataScriptPath = "$PSScriptRoot/Update-Pandoc-Metadata.ps1"
@@ -22,6 +16,14 @@ $MetadataScriptPath = "$PSScriptRoot/Update-Pandoc-Metadata.ps1"
 & $MetadataScriptPath
 
 Write-Host "Metadata update is complete." -ForegroundColor Green
+
+Write-Host "Fixing encoding ..."
+
+$EncodingScriptPath = "$PSScriptRoot/../scripts/Test-Encoding.ps1"
+
+& $EncodingScriptPath -Autofix
+
+Write-Host "Fix encoding is complete." -ForegroundColor Green
 
 Write-Host "Exit Code: $LASTEXITCODE" -ForegroundColor Green
 Write-Host "Changing Powershell Directory to $InitialDirectory ... " -ForegroundColor Green
